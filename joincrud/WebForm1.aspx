ff816839013ce13db8d8cf5ac6109bfcdd62dded
jaydip makwana <jmakwana732@rku.ac.in>
2023-05-04 09:27:01 +0530

    final commit

---
+++ joincrud/WebForm1.aspx
@@ -0,0 +1,85 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="joincrud.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <center>
+                  <asp:Label ID="orderid" runat="server" Text="Order Id"></asp:Label>
+                  <asp:TextBox ID="txtorderid" runat="server"></asp:TextBox>
+                  <asp:Button ID="btnsearch" runat="server" Text="Search" OnClick="btnsearch_Click" />
+                  <br />
+                <asp:Label ID="lblcustid" runat="server" Text="Customer Id"></asp:Label>
+                <asp:DropDownList ID="ddcustid" runat="server">
+                    <asp:ListItem>1</asp:ListItem>
+                    <asp:ListItem>2</asp:ListItem>
+                    <asp:ListItem>3</asp:ListItem>
+                  </asp:DropDownList>
+                <br />
+                <asp:Label ID="lblname" runat="server" Text="Cust Name"></asp:Label>
+                <asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+                <br />
+                 <asp:Label ID="lblorderdate" runat="server" Text="OrderDate"></asp:Label>
+                <asp:TextBox ID="txtorderdate" runat="server"></asp:TextBox>
+                <br />
+                 <asp:Label ID="lblshipdate" runat="server" Text="Ship Date"></asp:Label>
+                <asp:TextBox ID="txtshipdate" runat="server"></asp:TextBox>
+                <br />
+                 <asp:Label ID="lblamt" runat="server" Text="Order Amount"></asp:Label>
+                <asp:TextBox ID="txtamt" runat="server"></asp:TextBox>
+                <br />
+                <asp:Label ID="lblpayment" runat="server" Text="Payment Mode"></asp:Label>
+                <asp:DropDownList ID="ddpayment" runat="server">
+                    <asp:ListItem>Gpay</asp:ListItem>
+                    <asp:ListItem>ATM</asp:ListItem>
+                    <asp:ListItem>UPI</asp:ListItem>
+                  </asp:DropDownList>
+                <br />
+                <asp:Label ID="lblremarks" runat="server" Text="Remarks"></asp:Label>
+                <asp:TextBox ID="txtremarks" runat="server"></asp:TextBox>
+                  <br />
+                <br />
+                <asp:Button ID="btninsert" runat="server" Text="Insert" OnClick="btninsert_Click" />
+                 &nbsp;&nbsp;&nbsp;
+                 <asp:Button ID="btnupdate" runat="server" Text="Update" OnClick="btnupdate_Click" />
+                 &nbsp;&nbsp;&nbsp;
+                 <asp:Button ID="btndelete" runat="server" Text="Delete" OnClick="btndelete_Click" />
+                 &nbsp;&nbsp;&nbsp;
+                 <asp:Button ID="btnview" runat="server" Text="View" OnClick="btnview_Click" />
+                  <br />
+                  <br />
+                  <asp:GridView ID="GridView1" runat="server">
+                  </asp:GridView>
+                  <br />
+                  <asp:DropDownList ID="ddsearch" runat="server">
+                      <asp:ListItem>--select query----</asp:ListItem>
+                      <asp:ListItem>Q1</asp:ListItem>
+                      <asp:ListItem>Q2</asp:ListItem>
+                      <asp:ListItem>Q3</asp:ListItem>
+                      <asp:ListItem>Q4</asp:ListItem>
+                      <asp:ListItem>Q5</asp:ListItem>
+                      <asp:ListItem>Q6</asp:ListItem>
+                      <asp:ListItem>Q7</asp:ListItem>
+                      <asp:ListItem>Q8</asp:ListItem>
+                      <asp:ListItem>Q9</asp:ListItem>
+                      <asp:ListItem>Q10</asp:ListItem>
+                  </asp:DropDownList>
+&nbsp;&nbsp;
+                  <asp:Button ID="btnsubmit" runat="server" OnClick="btnsubmit_Click" Text="Submit" />
+                  <br />
+                  <br />
+                  <br />
+                  <asp:GridView ID="GridView2" runat="server">
+                  </asp:GridView>
+                  <br />
+
+            </center>
+        </div>
+    </form>
+</body>
+</html>
---
+++ joincrud/WebForm1.aspx.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace joincrud
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string str = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\MCA\MCA_2\BD.net\joincrud\joincrud\joincrud\App_Data\Database1.mdf;Integrated Security=True";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            FilldName();
+        }
+
+        protected void btninsert_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(str);
+            con.Open();
+            string query = "insert into Orders Values(@custid,@orderdate,@shipdate,@orderamt,@paymentmode,@remarks)";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@custid", ddcustid.SelectedValue);
+            cmd.Parameters.AddWithValue("@orderdate", txtorderdate.Text);
+            cmd.Parameters.AddWithValue("@shipdate", txtshipdate.Text);
+            cmd.Parameters.AddWithValue("@orderamt", txtamt.Text);
+            cmd.Parameters.AddWithValue("@paymentmode", ddpayment.SelectedValue); 
+            cmd.Parameters.AddWithValue("@remarks", txtremarks.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Response.Write("<script>alert('insert successfully....')</script>");
+        }
+
+        protected void btnupdate_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(str);
+            con.Open();
+            string query = "update Orders set custid=@custid,orderdate=@orderdate,shipdate=@shipdate,orderamt=@orderamt,paymentmode=@paymentmode,remarks=@remarks where  orderid=@orderid";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@custid", ddcustid.SelectedValue);
+            cmd.Parameters.AddWithValue("@orderdate", txtorderdate.Text);
+            cmd.Parameters.AddWithValue("@shipdate", txtshipdate.Text);
+            cmd.Parameters.AddWithValue("@orderamt", txtamt.Text);
+            cmd.Parameters.AddWithValue("@paymentmode", ddpayment.SelectedValue);
+            cmd.Parameters.AddWithValue("@remarks", txtremarks.Text);
+            cmd.Parameters.AddWithValue("@orderid", txtorderid.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Response.Write("<script>alert('update successfully....')</script>");
+        }
+
+        protected void btndelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(str);
+            con.Open();
+            string query = "delete from Orders where orderid=@orderid";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@orderid", txtorderid.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Response.Write("<script>alert('Record Deleted.....')</script>");
+        }
+
+        protected void btnsearch_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(str);
+            con.Open();
+            string query = "select * from Orders where orderid='"+txtorderid.Text+"'";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if(dr.Read())
+            {
+                ddcustid.SelectedValue = dr["custid"].ToString();
+                txtorderdate.Text = dr["orderdate"].ToString();
+                txtshipdate.Text = dr["shipdate"].ToString();
+                txtamt.Text = dr["orderamt"].ToString();
+                ddpayment.SelectedValue = dr["paymentmode"].ToString();
+                txtremarks.Text = dr["remarks"].ToString();
+            }
+            con.Close();
+        }
+        public void FilldName()
+        {
+            SqlConnection con = new SqlConnection(str);
+            con.Open();
+            string query = "select custid from Customer";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while(dr.Read())
+            {
+                ddcustid.Items.Add(dr[0].ToString());
+            }
+            con.Close();
+        }
+
+        protected void btnview_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(str);
+            con.Open();
+            string query = "select o.orderid,o.custid,c.name,c.city,c.state,o.orderamt from Customer c,Orders o where c.custid=o.custid";
+            SqlDataAdapter adpt = new SqlDataAdapter(query, con);
+            DataSet ds = new DataSet();
+            adpt.Fill(ds);
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+            con.Close();
+
+
+        }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            if(ddsearch.SelectedValue=="Q1")
+            {
+                //Display all orders with orderid, custid, customer name, city, state, order amount.
+                SqlConnection con = new SqlConnection(str);
+                con.Open();
+                string query = "select o.orderid,o.custid,c.name,c.city,c.state,o.orderamt from Customer c,Orders o where c.custid=o.custid ";
+                SqlDataAdapter adpt = new SqlDataAdapter(query, con);
+                DataSet ds = new DataSet();
+                adpt.Fill(ds);
+                GridView2.DataSource = ds;
+                GridView2.DataBind();
+                con.Close();
+
+            }
+            else if(ddsearch.SelectedValue=="Q2")
+            {
+                //) Display all orders with OrderID, CustID, customer name, city, state with order amount greater than 10000.
+                SqlConnection con = new SqlConnection(str);
+                con.Open();
+                string query = "select o.orderid,o.custid,c.name,c.city,c.state,o.orderamt from Customer c,Orders o where c.custid=o.custid and o.orderamt>11000";
+                SqlDataAdapter adpt = new SqlDataAdapter(query, con);
+                DataSet ds = new DataSet();
+                adpt.Fill(ds);
+                GridView2.DataSource = ds;
+                GridView2.DataBind();
+                con.Close();
+            }
+            else if(ddsearch.SelectedValue=="Q3")
+            {
+                //Display customer details with total order amount
+                SqlConnection con = new SqlConnection(str);
+                con.Close();
+                string query = "select sum(o.orderamt) from Orders o GROUP BY o.custid";
+                SqlDataAdapter adpt = new SqlDataAdapter(query, con);
+                DataSet ds = new DataSet();
+                adpt.Fill(ds);
+                GridView2.DataSource = ds;
+                GridView2.DataBind();
+                con.Close();
+
+            }
+            else if(ddsearch.SelectedValue=="Q4")
+            {
+                //Display CustID, name, OrderAmount by Month and Year
+                SqlConnection con = new SqlConnection(str);
+                con.Open();
+                string query = "select * from Customer c,Orders o where c.custid=o.custid ORDER BY YEAR(o.orderdate),MONTH(o.orderdate) ";
+                SqlDataAdapter adpt = new SqlDataAdapter(query, con);
+                DataSet ds = new DataSet();
+                adpt.Fill(ds);
+                GridView2.DataSource = ds;
+                GridView2.DataBind();
+                con.Close();
+            }
+            else if(ddsearch.SelectedValue=="Q5")
+            {
+                SqlConnection con = new SqlConnection(str);
+                con.Open();
+                string query = "select paymentmode,SUM(orderamt) from Orders GROUP BY paymentmode";
+                SqlDataAdapter adpt = new SqlDataAdapter(query, con);
+                DataSet ds = new DataSet();
+                adpt.Fill(ds);
+                GridView2.DataSource = ds;
+                GridView2.DataBind();
+                con.Close();
+            }
+            else if(ddsearch.SelectedValue=="Q6")
+            {
+                SqlConnection con = new SqlConnection(str);
+                con.Open();
+                string query = "select Orders.orderid,Customer.name from Orders INNER JOIN Customer ON Orders.custid=Customer.custid ";
+                SqlDataAdapter adpt = new SqlDataAdapter(query, con);
+                DataSet ds = new DataSet();
+                adpt.Fill(ds);
+                GridView2.DataSource = ds;
+                GridView2.DataBind();
+                con.Close();
+            }
+            else if(ddsearch.SelectedValue=="Q7")
+            {
+                SqlConnection con = new SqlConnection(str);
+                con.Open();
+                string query = "select Customer.name,Orders.orderid from Customer left join Orders on Customer.custid=Orders.custid order by Customer.name";
+                SqlDataAdapter adpt = new SqlDataAdapter(query, con);
+                DataSet ds = new DataSet();
+                adpt.Fill(ds);
+                GridView2.DataSource = ds;
+                GridView2.DataBind();
+                con.Close();
+            }
+
+        }
+    }
+}
